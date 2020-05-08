588cf028623bbbf2e73e5b759e1f3856946d9072
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-08 15:32:17 -0400

    added popper.js for toggle button to work

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+// import Portfolio from "./pages/Portfolio";
+// import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header />
-        <Route exact path="/" component={About} />
-        {/* <Route exact path="/portfolio" component={Portfolio} />
+      <Header />
+      <Route exact path="/" component={About} />
+      {/* <Route exact path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} /> */}
-      </div>
-    </Router>
+      <Footer />
+    </Router >
   )
 }
 
