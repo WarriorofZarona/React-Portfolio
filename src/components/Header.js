45f34d4d173c4e8632594978f2931905afedb41b
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-08 15:26:01 -0400

    added code for Navlinks with Router, imported to Header, displaying perfectly

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavWrapper from "./NavWrapper";
 import Logo from "./Logo";
 import TogglerBtn from "./TogglerBtn";
+import NavLinks from "./NavLinks";
 
 function Header() {
     return (
@@ -9,24 +10,7 @@ function Header() {
             <NavWrapper>
                 <Logo />
                 <TogglerBtn />
-
-                {/* 
-                <!-- Nav Links, when collapsed it becomes a dropdown menu--> */}
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.html"><i
-                                class="far fa-address-card mr-1"></i>About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="portfolio.html"><i
-                                class="fas fa-project-diagram mr-1"></i>Portfolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact.html"><i class="far fa-envelope mr-1"></i>Contact</a>
-                        </li>
-                    </ul>
-                </div>
+                <NavLinks />
             </NavWrapper>
         </header>
     )
