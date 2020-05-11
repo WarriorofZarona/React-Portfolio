8bf9854c0e8e3bf49d33fd5c5350b33876d021b8
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-11 17:09:20 -0400

    added SideContent which will house Profilepicture and Doclinks

---
+++ src/pages/SideContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SideContent() {
+    return (
+        <div className="col-lg-3">
+            {/* <ProfilePicture /> 
+<DocLinks />*/}
+        </div>
+    )
+};
+
+export default SideContent;
