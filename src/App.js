158a005e544b8bcd3d0a5ad8cf2e14d4eb91a398
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-13 00:10:18 -0400

    removed stray code in app.jss

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import PageTitle from "./components/PageTitle";
 import Footer from "./components/Footer";
+import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import About from "./pages/About";
 import "./App.css";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
       <PageTitle>
         <Route exact path="/" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} /> */}
+        <Route exact path="/contact" component={Contact} />
       </PageTitle>
       <Footer />
     </Router >
