3cddbc1990dc8aa1f30bde794eed19ba472adeeb
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-12 21:27:24 -0400

    commented out about components in app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import PageTitle from "./components/PageTitle";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Footer from "./components/Footer";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -14,7 +14,7 @@ function App() {
     <Router>
       <Header />
       <PageTitle>
-        <Route exact path="/" component={About} />
+        {/* <Route exact path="/" component={About} /> */}
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
       </PageTitle>
