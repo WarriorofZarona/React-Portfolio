ed9c7192e812c2eb69f7948bb0fd2ec083b5b346
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-13 09:52:45 -0400

    changed from p tags to proper br

--- src/components/DocLinks.js
+++ src/components/DocLinks.js
@@ -4,15 +4,15 @@ import Resume from "../assets/docs/Resume.pdf"
 function DocLinks() {
     return (
         <ul className="list-unstyled">
-            <li><p><a href={Resume}><button type="button" className="btn btn-info rounded"><i
+            <li><a href={Resume}><button type="button" className="btn btn-info rounded"><i
                 className="far fa-file">&nbsp;</i>
-                    Resume</button></a></p></li>
-            <li><p><a href="https://github.com/warriorofzarona"><button type="button"
+                    Resume</button></a></li>      <br />
+            <li><a href="https://github.com/warriorofzarona"><button type="button"
                 className="btn btn-info rounded"><i className="fab fa-github">&nbsp;</i>
-                    Github</button></a></p></li>
-            <li><p><a href="https://www.linkedin.com/in/andrewbautista85/"><button type="button"
+                    Github</button></a></li>      <br />
+            <li><a href="https://www.linkedin.com/in/andrewbautista85/"><button type="button"
                 className="btn btn-info rounded"><i className="fab fa-linkedin-in">&nbsp;</i>
-                    LinkedIn</button></a></p></li>
+                    LinkedIn</button></a></li>
         </ul>
     )
 };
