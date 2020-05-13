86c9a46f639e0f7b1a87146dbbd2fb42bd38da61
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-13 14:10:25 -0400

    added 3 total buttons with working button links

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -18,19 +18,23 @@ function Contact() {
             </RowContainer>
             <br />
             <RowContainer>
-                <div className="col-lg-12 text-secondary">
-                    <a href="mailto:andrewbautista85@gmail.com">
-                        <button type="button" className="btn btn-info btn-circle btn-md"><i className="far fa-envelope"></i></button></a>&nbsp;&nbsp;
-              andrewbautista85@gmail.com
-          </div>
+                <ColSpacer />
+                <ColSpacer />
+                <div className="col-lg-6 text-secondary">
+                    <a href="https://www.linkedin.com/in/andrewbautista85/">
+                        <button type="button" className="btn btn-info btn-circle btn-xl float-left"><i className="fab fa-linkedin-in"></i></button></a>&nbsp;&nbsp;
+                <h5>&nbsp;&nbsp;&nbsp;&nbsp;linkedin.com/in/andrewbautista85</h5>
+                </div>
             </RowContainer>
             <br />
             <RowContainer>
-                <div className="col-lg-12 text-secondary">
-                    <a href="mailto:andrewbautista85@gmail.com">
-                        <button type="button" className="btn btn-info btn-circle btn-md"><i className="far fa-envelope"></i></button></a>&nbsp;&nbsp;
-            andrewbautista85@gmail.com
-        </div>
+                <ColSpacer />
+                <ColSpacer />
+                <div className="col-lg-6 text-secondary">
+                    <a href="https://twitter.com/AMBWebDev">
+                        <button type="button" className="btn btn-info btn-circle btn-xl float-left"><i className="fab fa-twitter"></i></button></a>&nbsp;&nbsp;
+                <h5>&nbsp;&nbsp;&nbsp;&nbsp;@AMBWebDev</h5>
+                </div>
             </RowContainer>
         </div>
     )
