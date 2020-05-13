f525435adbfa7199b0247fc85b6992fa3d07947f
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-12 21:57:22 -0400

    added pages back

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import PageTitle from "./components/PageTitle";
-// import About from "./pages/About";
+import About from "./pages/About";
 import Footer from "./components/Footer";
-// import Portfolio from "./pages/Portfolio";
-// import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 import "./App.css";
 
 function App() {
@@ -14,9 +14,9 @@ function App() {
     <Router>
       <Header />
       <PageTitle>
-        {/* <Route exact path="/" component={About} />
+        <Route exact path="/" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} /> */}
+        <Route exact path="/contact" component={Contact} />
       </PageTitle>
       <Footer />
     </Router >
