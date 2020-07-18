65d28045cdd11257bce4a75f8191afdab2b77362
Andrew Bautista <andrewbautista85@gmail.com>
2020-07-18 19:50:07 -0400

    added project descriptions and accomplishments per app

--- src/utils/Projects.js
+++ src/utils/Projects.js
@@ -19,7 +19,7 @@ export const ProjectsArr = [
     {
         id: 0,
         title: "Password Generator",
-        description: "A user is prompted with criteria of character length and types of characters, and the password generator will randomly generate a password string the user can use.",
+        description: "A user is prompted with criteria of character length and types of characters, and the password generator will randomly generate a password string the user can use. My first Javascript app.",
         languages: ["HTML", "CSS", "Javascript"],
         image: PasswordGenerator,
         github: "https://github.com/warriorofzarona/Password-Generator/",
@@ -29,7 +29,7 @@ export const ProjectsArr = [
     {
         id: 1,
         title: "Coding Quiz",
-        description: "A timed multiple-choice quiz themed around Javascript. Answer correctly to score points. Wrong answers take 10 seconds off the timer.The user can save their initials and score to a High Score List.",
+        description: "A timed multiple-choice quiz themed around Javascript. Answer correctly to score points. Wrong answers take 10 seconds off the timer.The user can save their initials and score to a High Score List. This project helped me learn DOMs and dynamic changes in page content.",
         languages: ["HTML", "CSS", "Bootstrap", "Javascript"],
         image: CodingQuiz,
         github: "https://github.com/warriorofzarona/Coding-Quiz/",
@@ -39,7 +39,7 @@ export const ProjectsArr = [
     {
         id: 2,
         title: "Day Planner",
-        description: "Displays current day schedule to add hourly events. Events are color-coded by time of day: past hour is in gray, current hour is in red, future hours are in green. Uses moment.js.",
+        description: "Displays current day schedule to add hourly events. Events are color-coded by time of day: past hour is in gray, current hour is in red, future hours are in green. Uses moment.js. My first project using jQuery.",
         languages: ["HTML", "CSS", "Bootstrap", "Javascript", "jQuery"],
         image: DayPlanner,
         github: "https://github.com/warriorofzarona/Day-Planner/",
@@ -49,7 +49,7 @@ export const ProjectsArr = [
     {
         id: 3,
         title: "Hungry Hub",
-        description: "A search web app that lets hungry users search for restaurants by location or search for recipes by ingredients. This was collaborated project. Uses Edamam and Zomato API's.",
+        description: "A search web app that lets hungry users search for restaurants by location or search for recipes by ingredients. This was collaborated project, and our first time utilizing API's. Uses Edamam and Zomato API's.",
         languages: ["HTML", "CSS", "Bootstrap", "Javascript", "jQuery"],
         image: HungryHub,
         github: "https://github.com/warriorofzarona/Hungry-Hub/",
@@ -59,7 +59,7 @@ export const ProjectsArr = [
     {
         id: 4,
         title: "Weather Dashboard",
-        description: "Search for a city to see local current weather information as well as a five day forecast. Uses openweathermap API.",
+        description: "Search for a city to see local current weather information as well as a five day forecast. Uses openweathermap API, which was a more complicated API which a lot of data, which I wanted to display cleanly for the user.",
         languages: ["HTML", "CSS", "Javascript", "jQuery"],
         image: WeatherDashboard,
         github: "https://github.com/warriorofzarona/Weather-Dashboard/",
@@ -69,7 +69,7 @@ export const ProjectsArr = [
     {
         id: 5,
         title: "Good README Generator",
-        description: "Node app that allows a user to quickly generate a README for projects.",
+        description: "Node app that allows a user to quickly generate a README for projects. My first backend project using Node.js, I still use this app today to generate my project README's.",
         languages: ["Javascript", "Node.js"],
         image: GoodReadMeGenerator,
         github: "https://github.com/warriorofzarona/Good-README-Generator/",
@@ -79,7 +79,7 @@ export const ProjectsArr = [
     {
         id: 6,
         title: "Team Template Generator",
-        description: "Node app that allows a team manager to quickly generate an HTML page listing their employees and basic information.",
+        description: "Node app that allows a team manager to quickly generate an HTML page listing their employees and basic information. My first app that helps render a page along with Node.js.",
         languages: ["HTML", "CSS", "Javascript", "jQuery", "Node.js"],
         image: TeamTemplate,
         github: "https://github.com/warriorofzarona/Team-Template/",
@@ -89,7 +89,7 @@ export const ProjectsArr = [
     {
         id: 7,
         title: "Note Taker",
-        description: "Allows a user to type in notes, save them in a list, retrieve when needed, and delete when they click the delete button.",
+        description: "Allows a user to type in notes, save them in a list, retrieve when needed, and delete when they click the delete button. My first Express app, where I learned the concept of CRUD.",
         languages: ["HTML", "CSS", "Javascript", "jQuery", "Node.js", "Express.js"],
         image: NoteTaker,
         github: "https://github.com/warriorofzarona/Note-Taker/",
@@ -99,7 +99,7 @@ export const ProjectsArr = [
     {
         id: 8,
         title: "Eat-Da-Burger!",
-        description: "Enter a burger, then choose to devour the burger. Burgers appear on the left until devoured, then appear on the right. Uses handlebars.js",
+        description: "Enter a burger, then choose to devour the burger. Burgers appear on the left until devoured, then appear on the right. Uses handlebars.js to render the page. My first app using mySQL.",
         languages: ["HTML", "CSS", "Javascript", "Node.js", "Express.js", "mySQL"],
         image: Burger,
         github: "https://github.com/warriorofzarona/burger/",
@@ -109,7 +109,7 @@ export const ProjectsArr = [
     {
         id: 9,
         title: "Workout Tracker",
-        description: "A user can keep track of exercises, with options to continue their last session or start a new one. Users can also see their overall exercise stats.",
+        description: "A user can keep track of exercises, with options to continue their last session or start a new one. Users can also see their overall exercise stats. My first app using MongoDB.",
         languages: ["HTML", "CSS", "Javascript", "Node.js", "Express.js", "MongoDB"],
         image: WorkoutTracker,
         github: "https://github.com/warriorofzarona/workout-tracker/",
@@ -119,7 +119,7 @@ export const ProjectsArr = [
     {
         id: 10,
         title: "Employee Directory",
-        description: "Business owners can use this app to quickly search and sort through their employee information. Uses RandomUser API.",
+        description: "Business owners can use this app to quickly search and sort through their employee information. Uses RandomUser API.  This is my first REACT project, showcasing input handling that filters the results in real time. ",
         languages: ["HTML", "CSS", "Javascript", "REACT.js", "Node.js"],
         image: EmployeeDirectory,
         github: "https://github.com/WarriorofZarona/employee-directory",
@@ -129,7 +129,7 @@ export const ProjectsArr = [
     {
         id: 11,
         title: "Google Books Search",
-        description: "Use the Search Bar to find books from Google Books, view them in the store, or save them for viewing later. Uses Google Books API.",
+        description: "Use the Search Bar to find books from Google Books, view them in the store, or save them for viewing later. Uses Google Books API. This is the first time creating a MERN stack, combining my knowledge of backend with React front end design.",
         languages: ["HTML", "CSS", "Javascript", "REACT.js", "Node.js", "Express.js", "MongoDB"],
         image: GoogleBooksSearch,
         github: "https://github.com/WarriorofZarona/Google-Books-Search/",
@@ -138,7 +138,7 @@ export const ProjectsArr = [
     }, {
         id: 12,
         title: "Waypoint App Messenger",
-        description: "Create a profile to search for other games with similar interests, then open a chat with them to create connections for multiplayer opportunities. This was collaborated project. Uses RAWG Games API.",
+        description: "Create a profile to search for other games with similar interests, then open a chat with them to create connections for multiplayer opportunities. This was collaborated project, with a messenger system using web sockets and a log in system with onboarding for a user's profile. Uses RAWG Games API.",
         languages: ["HTML", "CSS", "Javascript", "REACT.js", "Node.js", "Express.js", "mySQL"],
         image: Waypoint,
         github: "https://github.com/WarriorofZarona/media-matcher",
@@ -147,7 +147,7 @@ export const ProjectsArr = [
     }, {
         id: 13,
         title: "Recipe Maker",
-        description: "Add a Recipe, then view it in a list. *First project outside of bootcamp, learning Angular*",
+        description: "Add a Recipe, then view it in a list. This is my first project past my bootcamp, and my first time creating an Angular app.",
         languages: ["HTML", "CSS", "Javascript", "Angular.js", "Node.js", "Express.js", "MongoDB"],
         image: RecipeMaker,
         github: "https://github.com/WarriorofZarona/Recipe-Maker",
