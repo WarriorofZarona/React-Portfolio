c61b935d694838bf4ae9e032d9fe74382d94630a
Warrior of Zarona <andrewbautista85@gmail.com>
2022-12-23 11:48:48 -0500

    feat: added Sidebar nav with basic scaffolding

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Sidebar = () => (
+
+    <nav style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 8, width: '17rem', height: '100vh', backgroundColor: 'lightblue' }}>
+        <div>Avatar here</div>
+        <div>Nav item here</div>
+        <div>Nav item here</div>
+        <div>Nav item here</div>
+        <div>Nav item here</div>
+        <div>Nav item here</div>
+    </nav>
+
+)
