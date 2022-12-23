615011ad9c649c81bff739dc35f87bcb0cc4d593
Warrior of Zarona <andrewbautista85@gmail.com>
2022-12-23 11:45:58 -0500

    feat: adding new Home component with renamed main branch

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Sidebar } from "../../components/Sidebar";
+
+export const Home = () => (
+    <div style={{ display: 'flex' }}>
+        <div>Nav here</div>
+        <div>Body here</div>
+    </div>
+);
